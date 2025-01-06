a356fed28f2a180a9fe3962e4b12d5a2c16962b0
NathanGrenier <nathangrenier01@gmail.com>
2025-01-06 18:59:29 -0500

    nathan: added github switch script

---
+++ switch-gituser/NathanGrenier.sh
@@ -0,0 +1,8 @@
+# Set your GitHub username and email
+USERNAME="NathanGrenier" # Make sure to remove the {}
+EMAIL="nathangrenier01@gmail.com" # Make sure to remove the {}
+
+git config user.name "$USERNAME"
+git config user.email "$EMAIL"
+
+echo "Git user has been set to: $USERNAME ($EMAIL)"
